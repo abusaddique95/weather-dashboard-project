083cd79c3ff48d3abe998b8b3e90d9d728fb1d97
abusaddique95 <abusaddique95@live.com>
2022-05-31 15:43:41 +0100

    updated template strings

--- assets/js/index.js
+++ assets/js/index.js
@@ -95,35 +95,41 @@ const renderCurrentData = (data) => {
   weatherInfoContainer.append(currentWeatherCard);
 };
 
-const renderForecastData = () => {
-  const forecastWeatherCards = `<div>
+const renderForecastData = (data) => {
+  const forecastWeatherCards = (each) => {
+    const forecast = `<div>
     <h2 class="text-center">5 day Forecast</h2>
     <HR />
     <div class="d-flex flex-row justify-content-center flex-wrap">
         <!-- card 1 -->
         <div class="card ms-2 mb-2 shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
             <div class="card-body">
-                <h5 class="card-title">Sunday, 29th May</h5>
+                <h5 class="card-title">${moment
+                  .unix(each.dt)
+                  .format("ddd, Do MMM")}</h5>
                 <div>
                     <div class="row g-0 text-center">
                         <div class="col-sm-12 bg-light">Temperature</div>
-                        <div class="col-sm-12">16 &deg;C</div>
+                        <div class="col-sm-12">${each.temp.day} &deg;C</div>
                     </div>
 
                     <div class="row g-0 text-center">
                         <div class="col-sm-12 bg-light">Humidity</div>
-                        <div class="col-sm-12">20 &percnt;</div>
+                        <div class="col-sm-12">${each.humidity} &percnt;</div>
                     </div>
 
                     <div class="row g-0 text-center">
                         <div class="col-sm-12 bg-light">Wind Speed</div>
-                        <div class="col-sm-12">35 mph</div>
+                        <div class="col-sm-12">${each.wind_speed} mph</div>
                     </div>
 
                     <div class="row g-0 text-center">
                         <div class="col-sm-12 bg-light">UV index</div>
                         <div class="col-sm-12">
-                            <span class="bg-success p-1 text-white">1</span>
+                            <span class="bg-success p-1 text-white"${getUviClassName(
+                              each.uvi
+                            )}"
+                                >${each.uvi}</span
                         </div>
                     </div>
                 </div>
@@ -245,6 +251,9 @@ const renderForecastData = () => {
                 </div>
             </div>
         </div>`;
+
+    return forecast;
+  };
 
   weatherInfoContainer.append(forecastWeatherCards);
 };
