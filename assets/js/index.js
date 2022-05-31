3baf9fa3caee6286069bb9482450d594ad79c3b8
abusaddique95 <abusaddique95@live.com>
2022-05-31 11:18:23 +0100

    render recent searches and handle form buttons complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+const recentSearchesContainer = $("recent-searches");
+
 const readFromLocalStorage = (key, defaultValue) => {
   // get from LS using key name
   const dataFromLS = localStorage.getItem(key);
@@ -12,6 +14,7 @@ const readFromLocalStorage = (key, defaultValue) => {
   }
 };
 
+// read and write local storage
 const writeToLocalStorage = (key, value) => {
   // convert value to string
   const stringifiedValue = JSON.stringify(value);
@@ -20,10 +23,69 @@ const writeToLocalStorage = (key, value) => {
   localStorage.setItem(key, stringifiedValue);
 };
 
-const onReady = () => {
-  const recentSearchesContainer = $("recent-searches");
-
+const renderRecentSearches = () => {
   const recentSearches = readFromLocalStorage("recentSearches", []);
+
+  if (recentSearches.length) {
+    const createRecentCity = (city) => {
+      return `<li class="list-group-item" data-city="${city}"
+        >
+        ${city}
+        </li>`;
+    };
+
+    const recentCities = recentSearches.map(createRecentCity).join;
+
+    const ul = `<ul class="list-group ${recentCities}">
+</ul>`;
+
+    recentSearchesContainer.append(ul);
+  } else {
+    const alert = `<div class="alert alert-warning" role="alert">
+        A simple warning alert—check it out!
+    </div>`;
+    recentSearchesContainer.append(alert);
+  }
 };
 
+const handleRecentSearchClick = async (event) => {
+  const target = $(event.target);
+
+  if (target.is("li")) {
+    const cityName = target.attr("data-city");
+  }
+};
+
+const handleFormSubmit = async (event) => {
+  event.preventDefault();
+
+  // get form input value
+  const cityName = $("#recent-searches").val();
+
+  // validate
+  if (cityName) {
+    const renderStatus = await renderWeatherInfo(cityName);
+
+    //  LS
+    const recentSearches = readFromLocalStorage("recentSearches", []);
+
+    if (!recentSearches.includes(cityName) && renderStatus) {
+      recentSearches.push(cityName);
+
+      writeToLocalStorage("recentSearches", recentSearches);
+
+      recentSearchesContainer.children().last().remove();
+
+      // re-render
+      renderRecentSearches();
+    }
+  }
+};
+
+const onReady = () => {
+  renderRecentSearches();
+};
+
+recentSearchesContainer.click(handleRecentSearchClick);
+searchForm.submit(handleFormSubmit);
 $(document).ready(onReady);
