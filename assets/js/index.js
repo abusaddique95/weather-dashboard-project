2d3ca814fb2cebd04adddc533a947c68723ae78b
abusaddique95 <abusaddique95@live.com>
2022-05-31 14:37:19 +0100

    render current and forecast data

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,6 +25,196 @@ const writeToLocalStorage = (key, value) => {
   localStorage.setItem(key, stringifiedValue);
 };
 
+const renderCurrentData = () => {
+  const currentWeatherCard = `<div class="text-center">
+    <h2>Wolverhampton</h2>
+    <h3>Sunday, 30th May 2022</h3>
+    <hr />
+    <img src="http://openweathermap.org/img/w/04d.png" alt="weather-icon">
+    </div>
+    <div>
+    <div class="row g-0">
+        <div class="col-sm-12 col-md-4 p-3">Temperature</div>
+        <div class="col-sm-12 col-md-8 p-3">16 &deg;C</div>
+    </div>
+
+    <div class="row g-0">
+        <div class="col-sm-12 col-md-4 p-3">Humidity</div>
+        <div class="col-sm-12 col-md-8 p-3">20 &percnt;</div>
+    </div>
+
+    <div class="row g-0">
+        <div class="col-sm-12 col-md-4 p-3">Wind Speed</div>
+        <div class="col-sm-12 col-md-8 p-3">35 mph</div>
+    </div>
+
+    <div class="row g-0">
+        <div class="col-sm-12 col-md-4 p-3">UV index</div>
+        <div class="col-sm-12 col-md-8 p-3">
+            <span class="bg-success p-2 text-white">1</span>
+        </div>
+    </div>
+
+    </div>
+    </div>`;
+
+  weatherInfoContainer.append(currentWeatherCard);
+};
+
+const renderForecastData = () => {
+  const forecastWeatherCards = `<div>
+    <h2 class="text-center">5 day Forecast</h2>
+    <HR />
+    <div class="d-flex flex-row justify-content-center flex-wrap">
+        <!-- card 1 -->
+        <div class="card ms-2 mb-2 shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Sunday, 29th May</h5>
+                <div>
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Temperature</div>
+                        <div class="col-sm-12">16 &deg;C</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Humidity</div>
+                        <div class="col-sm-12">20 &percnt;</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Wind Speed</div>
+                        <div class="col-sm-12">35 mph</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">UV index</div>
+                        <div class="col-sm-12">
+                            <span class="bg-success p-1 text-white">1</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- card 2 -->
+        <div class="card ms-2 mb-2 shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Sunday, 29th May</h5>
+                <div>
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Temperature</div>
+                        <div class="col-sm-12">16 &deg;C</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Humidity</div>
+                        <div class="col-sm-12">20 &percnt;</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Wind Speed</div>
+                        <div class="col-sm-12">35 mph</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">UV index</div>
+                        <div class="col-sm-12">
+                            <span class="bg-success p-1 text-white">1</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- card 3 -->
+        <div class="card ms-2 mb-2 shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Sunday, 29th May</h5>
+                <div>
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Temperature</div>
+                        <div class="col-sm-12">16 &deg;C</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Humidity</div>
+                        <div class="col-sm-12">20 &percnt;</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Wind Speed</div>
+                        <div class="col-sm-12">35 mph</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">UV index</div>
+                        <div class="col-sm-12">
+                            <span class="bg-success p-1 text-white">1</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- card 4 -->
+        <div class="card ms-2 mb-2 shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Sunday, 29th May</h5>
+                <div>
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Temperature</div>
+                        <div class="col-sm-12">16 &deg;C</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Humidity</div>
+                        <div class="col-sm-12">20 &percnt;</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Wind Speed</div>
+                        <div class="col-sm-12">35 mph</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">UV index</div>
+                        <div class="col-sm-12">
+                            <span class="bg-success p-1 text-white">1</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- card 5 -->
+        <div class="card ms-2 mb-2 shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Sunday, 29th May</h5>
+                <div>
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Temperature</div>
+                        <div class="col-sm-12">16 &deg;C</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Humidity</div>
+                        <div class="col-sm-12">20 &percnt;</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">Wind Speed</div>
+                        <div class="col-sm-12">35 mph</div>
+                    </div>
+
+                    <div class="row g-0 text-center">
+                        <div class="col-sm-12 bg-light">UV index</div>
+                        <div class="col-sm-12">
+                            <span class="bg-success p-1 text-white">1</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+  weatherInfoContainer.append(forecastWeatherCards);
+};
+
 const renderRecentSearches = () => {
   // get recent searches from LS
   const recentSearches = readFromLocalStorage("recentSearches", []);
@@ -53,6 +243,17 @@ const renderRecentSearches = () => {
     // append to parent
     recentSearchesContainer.append(alert);
   }
+};
+
+const renderErrorAlert = () => {
+  // empty container
+  weatherInfoContainer.empty();
+
+  const alert = `<div class="alert alert-danger" role="alert">
+      Something went wrong. Please try again.
+    </div>`;
+
+  weatherInfoContainer.append(alert);
 };
 
 const renderWeatherInfo = async (cityName) => {
@@ -110,7 +311,6 @@ const getWeatherData = async (cityName) => {
 const handleRecentSearchClick = async (event) => {
   const target = $(event.target);
 
-  // restrict clicks only from li
   if (target.is("li")) {
     // get data city attribute
     const cityName = target.attr("data-city");
