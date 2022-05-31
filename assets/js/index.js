5b61bbc333d042994d05ec20c2dcf1cf46473493
abusaddique95 <abusaddique95@live.com>
2022-05-31 12:45:37 +0100

    render weather info. addded api keys n url

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
-const recentSearchesContainer = $("recent-searches");
+const recentSearchesContainer = $("#recent-searches-container");
+const weatherInfoContainer = $("#weather-info-container");
+const searchForm = $("#search-form");
 
 const readFromLocalStorage = (key, defaultValue) => {
   // get from LS using key name
@@ -24,35 +26,96 @@ const writeToLocalStorage = (key, value) => {
 };
 
 const renderRecentSearches = () => {
+  // get recent searches from LS
   const recentSearches = readFromLocalStorage("recentSearches", []);
 
+  // ["foo", "bar"]
   if (recentSearches.length) {
     const createRecentCity = (city) => {
-      return `<li class="list-group-item" data-city="${city}"
-        >
-        ${city}
-        </li>`;
+      return `<li class="list-group-item" data-city="${city}"> ${city}
+      </li>`;
     };
 
-    const recentCities = recentSearches.map(createRecentCity).join;
+    const recentCities = recentSearches.map(createRecentCity).join("");
 
-    const ul = `<ul class="list-group ${recentCities}">
-</ul>`;
+    const ul = `<ul class="list-group rounded-0">
+        ${recentCities}
+      </ul>`;
 
+    // append to parent
     recentSearchesContainer.append(ul);
   } else {
+    // else empty show alert
     const alert = `<div class="alert alert-warning" role="alert">
-        A simple warning alert—check it out!
-    </div>`;
+        You have no recent searches.
+      </div>`;
+
+    // append to parent
     recentSearchesContainer.append(alert);
   }
+};
+
+const renderWeatherInfo = async (cityName) => {
+  try {
+    const weatherData = await getWeatherData(cityName);
+
+    windowInfoContainer.empty();
+
+    renderCurrentData(weatherData);
+
+    renderForecastData(weatherData);
+
+    return true;
+  } catch (error) {
+    renderErrorAlert();
+    return false;
+  }
+};
+
+const getWeatherData = async (cityName) => {
+  const currentDataUrl = constructUrl(
+    "https://api.openweathermap.org/data/2.5/weather",
+    {
+      q: cityName,
+      appid: "9828c3cfd73cc833c0599fbbda617050",
+    }
+  );
+
+  const currentData = await fetchData(currentDataUrl);
+
+  const lat = currentData?.coord?.lat;
+  const lon = currentData?.coord?.lon;
+  const displayCityName = currentData?.name;
+
+  // forecast url
+  const forecastDataUrl = constructUrl(
+    "https://api.openweathermap.org/data/2.5/onecall",
+    {
+      lat: lat,
+      lon: lon,
+      exclude: "minutely,hourly",
+      units: "metric",
+      appid: "9828c3cfd73cc833c0599fbbda617050",
+    }
+  );
+
+  const forecastData = await fetchData(forecastDataUrl);
+
+  return {
+    cityName: displayCityName,
+    weatherData: forecastData,
+  };
 };
 
 const handleRecentSearchClick = async (event) => {
   const target = $(event.target);
 
+  // restrict clicks only from li
   if (target.is("li")) {
+    // get data city attribute
     const cityName = target.attr("data-city");
+
+    await renderWeatherInfo(cityName);
   }
 };
 
@@ -60,23 +123,27 @@ const handleFormSubmit = async (event) => {
   event.preventDefault();
 
   // get form input value
-  const cityName = $("#recent-searches").val();
+  const cityName = $("#search-input").val();
 
   // validate
   if (cityName) {
+    // render weather cards
     const renderStatus = await renderWeatherInfo(cityName);
 
-    //  LS
+    // get recentSearches from LS
     const recentSearches = readFromLocalStorage("recentSearches", []);
 
     if (!recentSearches.includes(cityName) && renderStatus) {
+      // push city name to array
       recentSearches.push(cityName);
 
+      // write recent searches to LS
       writeToLocalStorage("recentSearches", recentSearches);
 
+      // remove previous items
       recentSearchesContainer.children().last().remove();
 
-      // re-render
+      // re-render recent cities
       renderRecentSearches();
     }
   }
